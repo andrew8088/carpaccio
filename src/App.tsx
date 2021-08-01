585507196ca251924d654836d2e93a5f0d682db4
Andrew Burgess <andrew@burgess.io>
2021-07-31 23:04:02 -0400

    localstorage support

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import { Task, DraftTask, TaskUtils } from "./Task";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
+const STORAGE_KEY = "my-tasks";
+
 export default function App() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasksState] = useState<Task[]>(
+    JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]")
+  );
+
+  const setTasks = (tasks: Task[]) => {
+    setTasksState(tasks);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
+  };
 
   const saveNewTask = (task: DraftTask) =>
     setTasks([...tasks, { ...task, id: TaskUtils.getId(tasks) }]);
@@ -12,6 +21,8 @@ export default function App() {
   return (
     <div className="main">
       <h1> Carpaccio.app </h1>
+
+      <button onClick={() => setTasks([])}> Clear State </button>
 
       <NewTaskForm saveNewTask={saveNewTask} />
 
